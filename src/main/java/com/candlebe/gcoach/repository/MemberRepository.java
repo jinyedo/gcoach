7e9523135a2d9198e743c06ef4a4ab6d3d788e10
jinyedo <whereyedo@naver.com>
2021-05-16 21:01:31 +0900

    불필요 쿼리 삭제

--- src/main/java/com/candlebe/gcoach/repository/MemberRepository.java
+++ src/main/java/com/candlebe/gcoach/repository/MemberRepository.java
@@ -3,10 +3,8 @@ package com.candlebe.gcoach.repository;
 import com.candlebe.gcoach.entity.Member;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
-import javax.transaction.Transactional;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
@@ -19,15 +17,5 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByUsername(String username);
 
     Optional<Member> findMemberByNickname(String nickname);
-
-//    @Modifying
-//    @Transactional
-//    @Query("UPDATE Member m SET m.nickname = :nickname WHERE m.username = :username")
-//    void updateNickname(String nickname, String username);
-//
-//    @Modifying
-//    @Transactional
-//    @Query("UPDATE Member m SET m.password = :password WHERE m.username = :username")
-//    void updatePassword(String password, String username);
 }
 
