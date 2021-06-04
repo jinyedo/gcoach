559c18b6524df23fd48f92592387ad48fd806177
jinyedo <whereyedo@naver.com>
2021-06-04 16:30:09 +0900

    TimeZone Asia/Seoul 설정

--- src/main/java/com/candlebe/gcoach/GcoachApplication.java
+++ src/main/java/com/candlebe/gcoach/GcoachApplication.java
@@ -1,13 +1,14 @@
 package com.candlebe.gcoach;
 
-import com.candlebe.gcoach.service.ContentService;
 import com.candlebe.gcoach.storage.StorageProperties;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import javax.annotation.PostConstruct;
+import java.util.Date;
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -16,6 +17,12 @@ public class GcoachApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GcoachApplication.class, args);
+	}
+
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+		System.out.println("현재 시간: " + new Date());
 	}
 
 //	@Bean
