66fc2e7fdfe65682a6cc2d5609555cde6665f7e6
jinyedo <whereyedo@naver.com>
2021-06-10 20:54:00 +0900

    로그인후 로그인 페이지 이동시 메인 페이지로 이동하도록 설정

--- src/main/java/com/candlebe/gcoach/controller/GcoachController.java
+++ src/main/java/com/candlebe/gcoach/controller/GcoachController.java
@@ -4,10 +4,12 @@ import com.candlebe.gcoach.dto.MemberDTO;
 import com.candlebe.gcoach.entity.Member;
 import com.candlebe.gcoach.entity.MemberRole;
 import com.candlebe.gcoach.repository.MemberRepository;
+import com.candlebe.gcoach.security.dto.AuthMemberDTO;
 import com.candlebe.gcoach.service.JoinService;
 import com.candlebe.gcoach.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,6 +22,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
+import java.util.Optional;
 
 @Controller
 @Log4j2
@@ -32,8 +35,16 @@ public class GcoachController {
     private final PasswordEncoder passwordEncoder;
 
     @GetMapping("/login")
-    public void login() {
-        log.info("login..........");
+    public String login(@AuthenticationPrincipal AuthMemberDTO authMemberDTO) {
+        try {
+            Optional<Member> members = memberRepository.findByUsername(authMemberDTO.getUsername(), authMemberDTO.isFormSocial());
+            if (members.isPresent()) {
+                return "redirect:/";
+            }
+        } catch (Exception e) {
+            return "login";
+        }
+        return "login";
     }
 
     @PostMapping("/login")
