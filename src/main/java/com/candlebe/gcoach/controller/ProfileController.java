ae756afcf7b255a44fd58e39cad21a415401f27f
jinyedo <whereyedo@naver.com>
2021-06-11 16:22:49 +0900

    프로젝트 1차 마무리

--- src/main/java/com/candlebe/gcoach/controller/ProfileController.java
+++ src/main/java/com/candlebe/gcoach/controller/ProfileController.java
@@ -16,10 +16,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
